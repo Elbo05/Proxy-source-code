83c687eb65c1d82475ae378f44e8a0cf7c2c625c
Elbo <74321157+Elbo05@users.noreply.github.com>
2022-10-09 14:36:22 +0300

    Add files via upload

---
+++ proxy/proxy.cpp
@@ -0,0 +1,84 @@
+#include <fstream>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <thread>
+#include "enet/include/enet.h"
+#include "http.h"
+#include "server.h"
+
+
+server* g_server = new server();
+//hosts
+using namespace std;
+BOOL WINAPI HandlerRoutine(DWORD dwCtrlType)
+{
+    printf("\nFixing Hosts File!");
+
+    try
+    {
+        std::ofstream dosyaYaz("C:\\Windows\\System32\\drivers\\etc\\hosts");
+
+        if (dosyaYaz.is_open()) {
+            dosyaYaz << "";
+            dosyaYaz.close();
+        }
+    }
+    catch (std::exception)
+    {
+    }
+    return FALSE;
+}
+
+
+
+
+
+
+int main() {
+#ifdef _WIN32
+    SetConsoleTitleA("proxy by Sahyui");
+    //add hosts guys
+    try
+    {
+        std::ofstream dosyaYaz("C:\\Windows\\System32\\drivers\\etc\\hosts");
+
+        if (dosyaYaz.is_open()) {
+            dosyaYaz << "";
+            dosyaYaz.close();
+        }
+    }
+    catch (std::exception)
+    {
+    }
+#endif
+
+    printf("\nProxy by Sahyui\n");
+    try
+    {
+        std::ofstream dosyaYaz("C:\\Windows\\System32\\drivers\\etc\\hosts");
+
+        if (dosyaYaz.is_open()) {
+            dosyaYaz << "127.0.0.1 growtopia1.com\n127.0.0.1 growtopia2.com";
+            dosyaYaz.close();
+        }
+    }
+    catch (std::exception)
+    {
+    }
+    std::thread http(http::run, "127.0.0.1", "17255");
+    http.detach();
+    printf("HTTP server is running.\n");
+
+    enet_initialize();
+    if (g_server->start()) {
+        printf("Server & client proxy is running.\n");
+        while (true) {
+            g_server->poll();
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+    }   
+    else
+        printf("Failed to start server or proxy.\n");
+}
